a8a3fc919f42a7e2f4cec4ba94b8f9ceedc81bdc
Munazzah <munazzah@gmail.com>
2023-11-02 13:04:46 +0530

    Demo for MavenProject

---
+++ src/main/java/SE_Group/MunazzahMavenProject1/App.java
@@ -0,0 +1,13 @@
+package SE_Group.MunazzahMavenProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Munaz!" );
+    }
+}
